3a93076aaed169b688ec8c3a42752d64244d6034
n4xwe <68827021+n4xwe@users.noreply.github.com>
2021-11-18 02:36:36 -0500

    Update rpi-bullseye-freedv-setup.sh

--- rpi-bullseye-freedv-setup.sh
+++ rpi-bullseye-freedv-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #install freedv (1.6.1) w/codec2 w/LPCNet 
-#N4XWE 9-28-2021
-#Visit http://www.iquadlabs.com
+#N4XWE 11-18-2021
+#Compiled on RaspiOS-bullseye dtd 2021-10-30 32-bit
 
 #Update the apt cache and upgrade the system packages to their latest versions
 sudo apt -y update && sudo apt -y upgrade
