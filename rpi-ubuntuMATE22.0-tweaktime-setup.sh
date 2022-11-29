3f97fbe11b7769fea16320ef6ff759c995c7ce28
n4xwe <68827021+n4xwe@users.noreply.github.com>
2022-11-29 10:32:12 -0600

    Add files via upload

---
+++ rpi-ubuntuMATE22.0-tweaktime-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#install tweaktime 
+#N4XWE 04-10-2022
+#Compiled on RaspiOS-bullseye dtd 2021-10-30 32-bit and RaspiOS-bullseye dtd 2022-1-28 32-bit
+
+
+
+#Update the apt cache and upgrade the system packages to their latest versions
+sudo apt update && sudo apt upgrade -y
+
+#Download and install the dependencies
+sudo apt -y install build-essential ||
+	{ echo 'Dependency installation failed'; exit 1; }
+
+#Create a unique directory for the tweaktime compile and make it the current directory
+mkdir -p ~/src/TWEAKTIME && cd ~/src/TWEAKTIME
+
+#Download the most recent version of the tweaktime source code
+wget -N https://kk5jy.net/tweaktime/Software/tweaktime-20200413a.tar.xz ||
+  { echo 'Unable to download the tweaktime source code file'; exit 1; }
+  
+#Extract the tweaktime source code files
+tar -xvf tweaktime-20200413a.tar.xz
+
+#Change the directory containing the tweaktime source code to the current directory
+cd ~/src/TWEAKTIME/tweaktime
+
+#Compile and install tweaktime
+make -j3 && sudo make install ||
+  { echo 'Unable to install tweaktime'; exit 1; }
+  
+
+  
+
+
+
+
