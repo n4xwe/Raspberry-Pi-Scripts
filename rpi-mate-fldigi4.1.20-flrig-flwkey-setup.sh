e06728278701ab0eecbd060294adb9da483fb8da
n4xwe <68827021+n4xwe@users.noreply.github.com>
2022-12-06 15:30:59 -0600

    Update rpi-mate-fldigi4.1.20-flrig-flwkey-setup.sh

--- rpi-mate-fldigi4.1.20-flrig-flwkey-setup.sh
+++ rpi-mate-fldigi4.1.20-flrig-flwkey-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #install fldigi(4.1.23) w/Hamlib(4.5) flrig(1.4.7) flwkey(1.2.3) 
 #N4XWE 12-05-2022
-#Tested on Ubuntu MATE 20.04LTS 64-bit
+#Test compiled on Ubuntu 22.04LTS 64-bit
 
 
 #Update the apt cache and upgrade the system packages to their latest versions
