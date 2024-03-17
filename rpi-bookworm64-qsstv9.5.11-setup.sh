74658769215ef84dc715c2487b14cd3a6a5e36f7
Dan Babcock <68827021+n4xwe@users.noreply.github.com>
2024-03-17 11:21:24 -0500

    Update rpi-bookworm64-qsstv9.5.11-setup.sh

--- rpi-bookworm64-qsstv9.5.11-setup.sh
+++ rpi-bookworm64-qsstv9.5.11-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #install QSSTV(9.5.11) w/HamLib(4.5.5)
 #N4XWE 12-18-2023
-#Test Compiled on RaspiOS-bookworm dtd 2023-12-05 64-bit
+#Test Compiled on Raspberry Pi OS Bookworm dtd 2024-03-12 64-bit
 
 
 #Update the apt cache and upgrade the system packages to their latest versions
